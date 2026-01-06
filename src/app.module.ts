748e55df2e328a4f4714b580aac5777cabb62c49
ihsanabuhanifah <ihsanabuhanifah@gmail.com>
2026-01-07 05:28:35 +0700

    first commit

---
+++ src/app.module.ts
@@ -0,0 +1,24 @@
+import { Module } from '@nestjs/common';
+import { AppController } from './app.controller';
+import { AppService } from './app.service';
+
+import { ConfigModule } from '@nestjs/config';
+import { TypeOrmModule } from '@nestjs/typeorm';
+
+@Module({
+  imports: [
+    ConfigModule.forRoot({
+      isGlobal: true,
+    }),
+    TypeOrmModule.forRootAsync({
+      useFactory: async () => {
+        const { typeOrmConfig } = await import('./config/typeorm.config.js');
+        return typeOrmConfig;
+      },
+    }),
+  ],
+  controllers: [AppController],
+  providers: [AppService],
+  exports: [],
+})
+export class AppModule {}
